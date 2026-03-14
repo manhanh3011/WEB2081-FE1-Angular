e772013ea47e65b2a781ec5076bef1d682b8905f
manhanh3011 <tranmanhanh3011@gmail.com>
2026-03-14 13:43:11 +0700

    Lab3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
+import { Products } from './pages/products/products';
+import { ProductDetails } from './pages/products/product-details/product-details';
+import { Stories } from './pages/stories/stories';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'about', component: About},
-    {path: 'contact', component: Contact}
+    {path: 'contact', component: Contact},
+    {path: 'products', component: Products},
+    {path: 'products/:slug', component: ProductDetails},
+    {path: 'stories', component: Stories},
 ];
---
+++ src/app/pages/stories/stories.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stories',
+  imports: [],
+  templateUrl: './stories.html',
+  styleUrl: './stories.css',
+})
+export class Stories {
+  stories: any[] = [
+    {
+      title: 'One Piece',
+      author: 'Oda',
+      views: 100000,
+      year: 1997,
+      genre: 'Adventure',
+      image: 'https://upload.wikimedia.org/wikipedia/vi/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg'
+    },
+    {
+      title: 'Naruto',
+      author: 'Kishimoto',
+      views: 90000,
+      year: 1999,
+      genre: 'Action',
+      image: 'https://hoanghamobile.com/tin-tuc/wp-content/uploads/2023/08/hinh-nen-naruto-29.jpg'
+    },
+    {
+      title: 'Doraemon',
+      author: 'Fujiko F Fujio',
+      views: 70000,
+      year: 1969,
+      genre: 'Comedy',
+      image: 'https://images2.thanhnien.vn/528068263637045248/2023/11/9/doraemon-dac-biet-16995066653581222955078.png'
+    },
+    {
+      title: 'Dragon Ball',
+      author: 'Akira Toriyama',
+      views: 95000,
+      year: 1984,
+      genre: 'Action',
+      image: 'https://photo.znews.vn/w660/Uploaded/piqbzcvo/2020_12_28/1.jpg'
+    },
+    {
+      title: 'Attack On Titan',
+      author: 'Hajime Isayama',
+      views: 85000,
+      year: 2009,
+      genre: 'Dark Fantasy',
+      image: 'https://genk.mediacdn.vn/k:2014/1-1414752165457/hinh-anh-dau-tien-cua-truyen-anh-attack-on-titan-phien-ban-viet-nam.jpg'
+    },
+    {
+      title: 'Bleach',
+      author: 'Tite Kubo',
+      views: 80000,
+      year: 2001,
+      genre: 'Action',
+      image: 'https://kenh14cdn.com/2016/1-1471532392849.jpg'
+    }
+  ];
+}
